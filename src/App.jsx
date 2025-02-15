9492845cf5b40aa4d4f46e6a3546d4495eac951b
keerthana290506 <keerthana290506.com>
2025-02-15 09:25:13 +0530

    Done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <div>
+      <h1>Keerthana.D</h1>
+      <p>Contact: keerthana.d.s67@kalvium.community</p>
+    </div>
+
+    </>
+  )
+}
+
+export default App
